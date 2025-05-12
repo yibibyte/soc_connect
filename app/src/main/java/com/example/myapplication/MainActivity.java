9c5e261036f43eeafc48b748e6242ed3fbcf233c
yibibyte <gnia@artcollege.ru>
2025-05-12 23:08:35 +0300

    git MainActive

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        Log.d("MyApp: MainActivity", "tag:MainActivity");
+        Log.d("MyApp: MainActivity", "message_tag:MainActivity");
     }
 
 }
